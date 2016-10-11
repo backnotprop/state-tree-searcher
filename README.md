7c4dd8217990325d595059be4e61ca98c398889f
Michael Ramos <mdramos8@gmail.com>
2016-10-10 23:51:01 -0400

    init and data

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # state-tree-searcher
+
+Proof of Concept - not using a document store purposefully for this
---
+++ data.js
@@ -0,0 +1,401 @@
+export default {
+    "0": {
+        "id": 0,
+        "label":  "Secretary of State",
+        "abv": "S",
+        "children": [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,50],
+        "chief": "Secretary of State"
+    },
+    "1": {
+        "id": 1,
+        "label":  "Counselor and Chief of Staff",
+        "abv": "S/COS",
+        "children": [],
+        "chief": "Counselor and Chief of Staff"
+    },
+    "2": {
+        "id": 2,
+        "label": "Deputy Secretary of State",
+        "abv": "D(B)",
+        "children": [],
+        "chief": "Deputy Secretary of State"
+    },
+    "3": {
+        "id": 3,
+        "label": "Deputy Secretary of State",
+        "abv": "D(N)",
+        "children": [4],
+        "chief": "Deputy Secretary of State"
+    },
+    "4": {
+        "id": 4,
+        "label": "Office of U.S. Foreign Assistance",
+        "abv": "F",
+        "children": [],
+        "chief": "Director"
+    },
+    "5": {
+        "id": 5,
+        "label": "Executive Secretariat",
+        "abv": "S/ES",
+        "children": [],
+        "chief": "Executive Secretary"
+    },
+    "6": {
+        "id": 6,
+        "label": "Intelligence and Research",
+        "abv": "INR",
+        "children": [],
+        "chief": "Assistant Secretary"
+    },
+    "7": {
+        "id": 7,
+        "label": "Legislative Affairs",
+        "abv": "H",
+        "children": [],
+        "chief": "Assistant Secretary"
+    },
+     "8": {
+        "id": 8,
+        "label": "Office of Legal Adviser",
+        "abv": "L",
+        "children": [],
+        "chief": "Legal Adviser"
+    },
+     "9": {
+        "id": 9,
+        "label": "Office of Inspector Genral",
+        "abv": "OIG",
+        "children": [],
+        "chief": "Inspector Genral"
+    },
+     "10": {
+        "id": 10,
+        "label": "Office of Policy Planning",
+        "abv": "S/P",
+        "children": [],
+        "chief": "Director"
+    },
+     "11": {
+        "id": 11,
+        "label": "Office of Chief Protocol",
+        "abv": "S/CPR",
+        "children": [],
+        "chief": "Ambassador"
+    },
+     "12": {
+        "id": 12,
+        "label": "Office of Civil Rights",
+        "abv": "S/OCR",
+        "children": [],
+        "chief": "Director"
+    },
+     "13": {
+        "id": 13,
+        "label": "Office of Global Women's Issues",
+        "abv": "S/GWI",
+        "children": [],
+        "chief": "Ambassador-at-Large"
+    },
+     "14": {
+        "id": 14,
+        "label": "Office of Global AIDS Coordinator",
+        "abv": "S/GAC",
+        "children": [],
+        "chief": "Ambassador-at-Large"
+    },
+     "15": {
+        "id": 15,
+        "label": "Special Envoys and Special Representatives",
+        "abv": "",
+        "children": [],
+        "chief": ""
+    },
+    "16": {
+        "id": 16,
+        "label": "Under Secretary for Political Affairs",
+        "abv": "P",
+        "children": [22,23,24,25,26,27,28],
+        "chief": "Under Secretary"
+    },
+    "17": {
+        "id": 17,
+        "label": "Under Secretary for Economic Growth, Energy and Enviornment",
+        "abv": "E",
+        "children": [29,30,31,32],
+        "chief": "Under Secretary"
+    },
+    "18": {
+        "id": 18,
+        "label": "Under Secretary for Arms and Control and International Security Affairs",
+        "abv": "T",
+        "children": [33,34,35],
+        "chief": "Under Secretary"
+    },
+    "19": {
+        "id": 19,
+        "label": "Under Secretary for Public Diplomacy and Public Affairs",
+        "abv": "R",
+        "children": [36,37,38],
+        "chief": "Under Secretary"
+    },
+    "20": {
+        "id": 20,
+        "label": "Under Secretary for Management",
+        "abv": "M",
+        "children": [39,40,41,42,43,44,45,46,47,48,49],
+        "chief": "Under Secretary"
+    },
+    "22": {
+        "id": 22,
+        "label": "African Affairs",
+        "abv": "AF",
+        "children": [],
+        "chief": "Assistant Secretary"
+    },
+    "23": {
+        "id": 23,
+        "label": "South and Central Asain Affairs",
+        "abv": "SCA",
+        "children": [],
+        "chief": "Assistant Secretary"
+    },
+    "24": {
+        "id": 24,
+        "label": "European and Eurasain Afairs",
+        "abv": "EUR",
+        "children": [],
+        "chief": "Assistant Secretary"
+    },
+    "25": {
+        "id": 25,
+        "label": "Western Hemisphere Affairs",
+        "abv": "WHA",
+        "children": [],
+        "chief": "Assistant Secretary"
+    },
+    "26": {
+        "id": 26,
+        "label": "East Asain and Pacific Affairs",
+        "abv": "EAP",
+        "children": [],
+        "chief": "Assistant Secretary"
+    },
+    "27": {
+        "id": 27,
+        "label": "International Organizations",
+        "abv": "IO",
+        "children": [],
+        "chief": "Assistant Secretary"
+    },
+    "28": {
+        "id": 28,
+        "label": "Near Eastern Affairs",
+        "abv": "NEA",
+        "children": [],
+        "chief": "Assistant Secretary"
+    },
+    "29": {
+        "id": 29,
+        "label": "Economic & Business Affairs",
+        "abv": "EB",
+        "children": [],
+        "chief": "Assistant Secretary"
+    },
+    "30": {
+        "id": 30,
+        "label": "Energy Resources",
+        "abv": "ENR",
+        "children": [],
+        "chief": "Assistant Secretary"
+    },
+    "31": {
+        "id": 31,
+        "label": "Oceans and International Enviornmental and Scientific Affairs",
+        "abv": "OES",
+        "children": [],
+        "chief": "Assistant Secretary"
+    },
+    "32": {
+        "id": 32,
+        "label": "Office of the Chief Economist",
+        "abv": "",
+        "children": [],
+        "chief": "Chief Economist"
+    },
+    "33": {
+        "id": 33,
+        "label": "Arms Control, Verification, and Compliance",
+        "abv": "AVC",
+        "children": [],
+        "chief": "Assistant Economist"
+    },
+    "34": {
+        "id": 34,
+        "label": "International Security and Nonproliferation",
+        "abv": "ISN",
+        "children": [],
+        "chief": "Assistant Secretary"
+    },
+    "35": {
+        "id": 35,
+        "label": "Political-Military Affairs",
+        "abv": "PM",
+        "children": [],
+        "chief": "Assistant Secretary"
+    },
+    "36": {
+        "id": 36,
+        "label": "Education and Cultural Affairs",
+        "abv": "ECA",
+        "children": [],
+        "chief": "Assistant Secretary"
+    },
+    "37": {
+        "id": 37,
+        "label": "International Information Programs",
+        "abv": "IIP",
+        "children": [],
+        "chief": "Coordinator"
+    },
+    "38": {
+        "id": 38,
+        "label": "Public Affairs",
+        "abv": "PA",
+        "children": [],
+        "chief": "Assistant Secretary"
+    },
+    "39": {
+        "id": 39,
+        "label": "Administration",
+        "abv": "A",
+        "children": [],
+        "chief": "Chief Economist"
+    },
+    "40": {
+        "id": 40,
+        "label": "Comptroller, Global Financial Services",
+        "abv": "CGFS",
+        "children": [],
+        "chief": "Director"
+    },
+    "41": {
+        "id": 41,
+        "label": "Budget and Planning",
+        "abv": "BP",
+        "children": [],
+        "chief": "Director"
+    },
+    "42": {
+        "id": 42,
+        "label": "Human Resources",
+        "abv": "HR",
+        "children": [],
+        "chief": "Director General of the Foreign Service and the Director of Human Service"
+    },
+    "43": {
+        "id": 43,
+        "label": "Consular Affairs",
+        "abv": "CA",
+        "children": [],
+        "chief": "Assistant Secretary"
+    },
+    "44": {
+        "id": 44,
+        "label": "Information and Resource Management",
+        "abv": "IRM",
+        "children": [],
+        "chief": "Cheif Information Officer"
+    },
+    "45": {
+        "id": 45,
+        "label": "Diplomatic Security and Foreign Missions",
+        "abv": "DS",
+        "children": [],
+        "chief": "Assistant Secretary"
+    },
+    "46": {
+        "id": 46,
+        "label": "Office of Medical Services",
+        "abv": "MED",
+        "children": [],
+        "chief": "Director"
+    },
+    "47": {
+        "id": 47,
+        "label": "Foreign Service Insitute",
+        "abv": "FSI",
+        "children": [],
+        "chief": "Director"
+    },
+    "48": {
+        "id": 48,
+        "label": "Overseas Buildings Operations",
+        "abv": "OBO",
+        "children": [],
+        "chief": "Director"
+    },
+    "49": {
+        "id": 49,
+        "label": "Office of Managment, Policy, Rightsizing, and Innovation",
+        "abv": "M/PRI",
+        "children": [],
+        "chief": "Director"
+    },
+    "50": {
+        "id": 50,
+        "label": "Under Secretary for Civilian Security, Democracy and Human Rights",
+        "abv": "J",
+        "children": [51,52,53,54,55,56,57],
+        "chief": "Under Secretary"
+    },
+    "51": {
+        "id": 51,
+        "label": "Conflict and Stabilization Operations",
+        "abv": "CSO",
+        "children": [],
+        "chief": "Assistant Secretary"
+    },
+    "52": {
+        "id": 52,
+        "label": "Counterterrorism",
+        "abv": "CT",
+        "children": [],
+        "chief": "Coordinator and Ambassador-at-Large"
+    },
+    "53": {
+        "id": 53,
+        "label": "Democracy, Human Rights, and Labor",
+        "abv": "DRL",
+        "children": [],
+        "chief": "Assistant Secretary"
+    },
+    "54": {
+        "id": 54,
+        "label": "International Narcotics and Law Enforcement",
+        "abv": "INL",
+        "children": [],
+        "chief": "Assistant Secretary"
+    },
+    "55": {
+        "id": 55,
+        "label": "Population, Refugees and Migration",
+        "abv": "PRM",
+        "children": [],
+        "chief": "Assistant Secretary"
+    },
+    "56": {
+        "id": 56,
+        "label": "Office to Monitor and Combat Tracking in Persons",
+        "abv": "TIP",
+        "children": [],
+        "chief": "Ambassador-at-Large"
+    },
+    "57": {
+        "id": 57,
+        "label": "Office of Global Criminal Justice",
+        "abv": "GCJ",
+        "children": [],
+        "chief": "Ambassador-at-Large"
+    }
+}
